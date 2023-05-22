eef2f717f9d0feb2546621dfd26f6f360465d223
Katja <katja.k.helenius@aalto.fi>
2023-05-22 09:33:53 +0300

    4.1-4.2

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+`mongodb+srv://helenik4:${password}@cluster0.6zshaow.mongodb.net/blogApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+  })
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const blog= new Blog({
+    title: 'This is blog',
+    author: 'Test Author',
+    url: 'url',
+    likes: 0
+  })
+
+//   blog.save().then(result => {
+//     console.log('blog saved!')
+//     mongoose.connection.close()
+//   })
+
+
+Blog.find({}).then(result => {
+  result.forEach(blog => {
+    console.log(blog)
+  })
+  mongoose.connection.close()
+})
